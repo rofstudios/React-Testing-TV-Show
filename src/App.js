7a993885cbed9482df15779de4f08a848078b9a9
Your Name <you@example.com>
2020-05-20 16:29:58 -0400

    mvp

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Dropdown from "react-dropdown";
 import parse from "html-react-parser";
+import {fetchShow} from './api/fetchShow';
 
 import { formatSeasons } from "./utils/formatSeasons";
 
@@ -15,17 +16,22 @@ export default function App() {
   const episodes = seasons[selectedSeason] || [];
 
   useEffect(() => {
-    const fetchShow = () => {
-      axios
-        .get(
-          "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
-        )
-        .then(res => {
-          setShow(res.data);
-          setSeasons(formatSeasons(res.data._embedded.episodes));
-        });
-    };
-    fetchShow();
+    // const fetchShow = () => {
+    //   axios
+    //     .get(
+    //       "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    //     )
+    //     .then(res => {
+    //       setShow(res.data);
+    //       setSeasons(formatSeasons(res.data._embedded.episodes));
+    //     });
+    // };
+    fetchShow()
+    .then(res => {
+      console.log(res.data, 'res app')
+      setShow(res.data);
+      setSeasons(formatSeasons(res.data._embedded.episodes));
+    });
   }, []);
 
   const handleSelect = e => {
@@ -42,12 +48,13 @@ export default function App() {
       <h1>{show.name}</h1>
       {parse(show.summary)}
       <Dropdown
+        data-testid='dropdown'
         options={Object.keys(seasons)}
         onChange={handleSelect}
         value={selectedSeason || "Select a season"}
         placeholder="Select an option"
       />
-      <Episodes episodes={episodes} />
+      <Episodes episodes={episodes} data-testid='episodes'/>
     </div>
   );
 }
